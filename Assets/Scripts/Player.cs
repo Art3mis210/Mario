66cd50c20da559545ea25e2dc6c46aa6e8f9a2c7
Art3mis210 <77666553+Art3mis210@users.noreply.github.com>
2021-02-23 10:49:34 +0530

    added movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+    public float speed = 10f;
+    private SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        // float v = Input.GetAxis("Vertical");
+        if(h!=0)
+          sr.flipX = h < 0 || !(h > 0);
+        rigidBody.velocity = new Vector2(h * speed, rigidBody.velocity.y);
+
     }
 }
